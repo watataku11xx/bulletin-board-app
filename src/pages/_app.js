55f75c5a9a99b0bea735a7caeb23ee9ad7cfdc28
watataku11xx <watataku131122@outlook.jp>
2023-08-27 01:07:21 +0900

    [add] id.jsに簡単なスタイリング、ローディング機能の追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
 import { SessionProvider } from 'next-auth/react'
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps:{session, ...pageProps} }) {
   return (
--- src/pages/tools/[id].js
+++ src/pages/tools/[id].js
@@ -1,33 +1,71 @@
 import PostComment  from '@components/PostComment'
 import NotLoginPage  from '@components/NotLoginPage'
 import { useSession } from 'next-auth/react';
+import { Container, Box, Button} from '@mui/material';
+import { useState } from 'react';
 
 function Id({tool, comment, post_id}){
+    const [isToggled, setIsToggled] = useState(false);
+    const handleToggle = () => {
+        setIsToggled((prev) => !prev);
+    };
 
-console.log(tool);
-
-  const { data: session } = useSession();
+    const { data: session } = useSession();
+    const originalDateString = tool.post.post_date;
+    const date = new Date(originalDateString);
+    const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
   
     return(
         <>
             {
                 session && (
                     <>
-                        <h2>{tool.post.title}</h2>
-                        <p>{tool.post.content}</p>
-                        <p>{tool.post.post_date}</p>
-                        {tool.categoryNames.map((category, index) => (
-                            <div key={index}>
-                                <span>{category}</span>
-                            </div>
-                        ))}
-                        <PostComment propValue={post_id}/>
-                        {comment.map((commentItem, index) => (
-                            <div key={index}>
-                                <p>評価: {commentItem.comment_value}</p>
-                                <p>コメント: {commentItem.comment_content}</p>
-                            </div>
-                        ))}
+                        <Container maxWidth='sm'>
+                            <Box sx={{
+                                border: 3,
+                                borderRadius: 5,
+                                padding: 2,
+                            }}>
+                                <h2>{tool.post.title}</h2>
+                                <p>登録日: {formattedDate}</p>
+                                <p>概要: {tool.post.content}</p>
+                                {tool.categoryNames.map((category, index) => (
+                                    <div key={index}>
+                                        <span>{category}</span>
+                                    </div>
+                                ))}
+                                {
+                                    isToggled && (
+                                        <Box sx={{
+                                        }}>
+                                            <PostComment propValue={post_id}/>
+                                        </Box>
+                                    )
+                                }
+                                {comment.map((commentItem, index) => (
+                                    <Box sx={{
+                                        padding: 1,
+                                        border: 2, 
+                                        borderRadius: 2,
+                                        margin: 1,
+                                    }}>
+                                        <div key={index}>
+                                            <p>評価: {commentItem.comment_value}</p>
+                                            <p>コメント: {commentItem.comment_content}</p>
+                                        </div>
+                                    </Box>
+                                ))}
+                            </Box>
+                        </Container>
+                        { !isToggled && (
+                            <Button sx={{
+
+                            }}
+                                onClick={handleToggle}
+                            >
+                                コメント追加
+                            </Button>
+                        )}
                     </>
                 )
             }
